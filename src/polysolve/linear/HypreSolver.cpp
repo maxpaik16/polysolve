089c20ce1b8c62a8c522dc54bb95cc475a55a0cc
maxpaik16 <maxpaik@gmail.com>
2024-11-12 13:28:20 -0500

    fix compiler error

--- src/polysolve/linear/HypreSolver.cpp
+++ src/polysolve/linear/HypreSolver.cpp
@@ -343,9 +343,9 @@ namespace polysolve::linear
         HYPRE_IJVectorDestroy(x);
     }
 
-    void mixed_direct_iterative_solve(const Ref<const VectorXd> b, Ref<VectorXd> x)
+    void HypreSolver::mixed_direct_iterative_solve(const Ref<const VectorXd> b, Ref<VectorXd> x)
     {
-        
+
     }
 
     ////////////////////////////////////////////////////////////////////////////////
