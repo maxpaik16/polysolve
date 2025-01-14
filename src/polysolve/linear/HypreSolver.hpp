d2e43395f1c5cf7b2e4bdbe5ee3d592fda72602b
maxpaik16 <maxpaik@gmail.com>
2025-01-14 14:36:45 -0500

    add direct update code

--- src/polysolve/linear/HypreSolver.hpp
+++ src/polysolve/linear/HypreSolver.hpp
@@ -51,7 +51,6 @@ namespace polysolve::linear
         // Solve the linear system Ax = b
         virtual void solve(const Ref<const VectorXd> b, Ref<VectorXd> x) override;
 
-        virtual void set_problematic_dofs(std::set<int> &bad_indices) override {bad_indices_ = bad_indices;}
 
         // Name of the solver type (for debugging purposes)
         virtual std::string name() const override { return "Hypre"; }
@@ -82,6 +81,8 @@ namespace polysolve::linear
     private:
         bool has_matrix_ = false;
         int precond_num_;
+
+        Eigen::MatrixXd eigen_A;
 
         HYPRE_IJMatrix A;
         HYPRE_ParCSRMatrix parcsr_A;
--- src/polysolve/nonlinear/descent_strategies/Newton.cpp
+++ src/polysolve/nonlinear/descent_strategies/Newton.cpp
@@ -210,6 +210,7 @@ namespace polysolve::nonlinear
                 return std::nan("");
             }
             linear_solver->set_positions(positions);
+            linear_solver->set_problematic_dofs(problematic_indices);
             linear_solver->solve(-grad, direction); // H Δx = -g
         }
 
@@ -251,6 +252,7 @@ namespace polysolve::nonlinear
                 linear_solver->analyze_pattern_dense(hessian, hessian.rows());
                 linear_solver->factorize_dense(hessian);   
                 linear_solver->set_positions(positions);
+                linear_solver->set_problematic_dofs(problematic_indices);
                 linear_solver->solve(-grad, direction);
             }
             catch (const std::runtime_error &err)
