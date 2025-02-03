a5bcac2ec8b6c04243b9b7d6404bfd76d7189aa9
maxpaik16 <maxpaik@gmail.com>
2025-02-03 15:49:05 -0500

    force on ichol

--- src/polysolve/linear/ExperimentalSolver.hpp
+++ src/polysolve/linear/ExperimentalSolver.hpp
@@ -12,6 +12,8 @@
 #include <HYPRE_parcsr_ls.h>
 #include <HYPRE_parcsr_mv.h>
 
+#define POLYSOLVE_WITH_ICHOL 1
+
 #if POLYSOLVE_WITH_ICHOL
 #include "cholesky.h"
 #include <boost/property_tree/ptree.hpp>
