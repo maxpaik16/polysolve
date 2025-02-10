87bce0c2e5ad840d10b6cdee26c306c30e9f78e6
maxpaik16 <maxpaik@gmail.com>
2025-02-10 11:57:59 -0500

    turn off mschol

--- cmake/recipes/mschol.cmake
+++ cmake/recipes/mschol.cmake
@@ -8,8 +8,9 @@ include(CPM)
 CPMAddPackage(
     NAME mschol
     GIT_REPOSITORY https://github.com/maxpaik16/mschol
-    GIT_TAG 3a4409a53877e21dcf97146c2d9bbcb1278411c1
+    GIT_TAG 8d8aa1a5fa219f5683ce81d6c1f0d84ba479f0e5
 )
 
 #add_library(mschol::mschol ALIAS mschol)
+target_link_libraries(mschol PRIVATE Eigen3::Eigen)
 target_include_directories(mschol SYSTEM PUBLIC ${mschol_SOURCE_DIR}/src)
