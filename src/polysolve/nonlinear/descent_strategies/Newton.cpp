2bdc4c5e6c02abaea7cd65f5db1347f9467cd3ea
maxpaik16 <maxpaik@gmail.com>
2025-09-23 10:16:08 -0400

    use regularized if ppn fails

--- src/polysolve/nonlinear/descent_strategies/Newton.cpp
+++ src/polysolve/nonlinear/descent_strategies/Newton.cpp
@@ -53,6 +53,13 @@ namespace polysolve::nonlinear
                 sparse, ppn_solver_params, linear_solver_params,
                 characteristic_length, logger));
 
+            const double reg_weight_min = solver_params["Newton"]["reg_weight_min"];
+            if (reg_weight_min > 0)
+                res.push_back(std::make_unique<RegularizedNewton>(
+                    sparse, solver_params["Newton"]["use_psd_projection_in_regularized"],
+                    reg_solver_params, linear_solver_params,
+                    characteristic_length, logger));
+
             return res;
         }
 
