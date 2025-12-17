da5066326e41e87c753d27ed866d018b733bceb4
maxpaik16 <maxpaik@gmail.com>
2025-12-16 23:58:29 -0800

    fix newton tol adaptivity logic

--- src/polysolve/nonlinear/descent_strategies/Newton.cpp
+++ src/polysolve/nonlinear/descent_strategies/Newton.cpp
@@ -247,7 +247,7 @@ namespace polysolve::nonlinear
             POLYSOLVE_SCOPED_STOPWATCH("linear solve", this->inverting_time, m_logger);
             if (use_adaptive_residual_tolerance)
             {
-                linear_solver->set_tolerance(std::max(grad.norm() / 10, residual_tolerance));
+                linear_solver->set_tolerance(residual_tolerance);
             }
             // TODO: get the correct siz
             linear_solver->analyze_pattern(hessian, hessian.rows());
